c71d3e0d5165a4d9e74606ae5ec09b9c00641a3b
shineyr <fly_yr2012@163.com>
2015-08-22 20:32:53 +0800

    Median of Two Sorted Arrays

--- LeetCode04/main.cpp
+++ LeetCode04/main.cpp
@@ -83,16 +83,15 @@ public:
 
 
 
-
 int main()
 {
-	//int arr1[] = { /*1, 2, 3, 3, 4, 5, 6*/ };
-	int arr2[] = { 2, 3/*, 4, 5, 6, 6, 7, 8*/ };
+	int arr1[] = { 1, 2, 3, 3, 4, 5, 6 };
+	int arr2[] = { 2, 3, 4, 5, 6, 6, 7, 8 };
 	vector<int> v1;
 	vector<int> v2(arr2 , arr2+2);
 
 	Solution s;
-	cout << s.findMedianSortedArrays(v1, v2) << endl;
+	cout << s.findMedianSortedArrays(arr1,7 ,  arr2 , 8) << endl;
 
 	system("pause");
 	return 0;
