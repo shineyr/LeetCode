8cba0e91281441c9ef13021bdb193bd17d3989ab
shineyr <fly_yr2012@163.com>
2015-08-31 20:13:44 +0800

    Permutations II

---
+++ LeetCode47/main.cpp
@@ -0,0 +1,37 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<vector<int>> permuteUnique(vector<int>& nums) {
+		vector<vector<int> > ret;
+
+		if (nums.empty())
+			return ret;
+
+		sort(nums.begin(), nums.end());
+		ret.push_back(nums);
+		while (next_permutation(nums.begin(), nums.end()))
+			ret.push_back(nums);
+
+		return ret;
+	}
+};
+
+int main()
+{
+	Solution s;
+	vector<vector<int> > v = s.permute(vector<int>{1, 1, 2});
+
+	for (int i = 0; i < v.size(); i++)
+		cout << v[i][0] << "\t" << v[i][1] << "\t" << v[i][2] << endl;
+
+	system("pause");
+
+	return 0;
+}
