70aa7689acdfca8602ef884fdbce1dff12aae36f
shineyr <fly_yr2012@163.com>
2015-11-15 12:08:00 +0800

    Update main.cpp

--- 120_Triangle/main.cpp
+++ 120_Triangle/main.cpp
@@ -30,7 +30,7 @@ public:
 					triangle[i][j] += triangle[i - 1][j - 1];
 				}
 				else{
-					triangle[i][j] = min(triangle[i][j] + triangle[i - 1][j - 1], triangle[i][j] + triangle[i - 1][j - 1]);
+					triangle[i][j] = min(triangle[i][j] + triangle[i - 1][j], triangle[i][j] + triangle[i - 1][j - 1]);
 				}//else
 			}//for
 		}//for
