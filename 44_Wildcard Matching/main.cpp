64422f60dfb42395b32f96f4da0412c21f5bfeb4
shineyr <fly_yr2012@163.com>
2015-12-15 14:49:56 +0800

    Wildcard Matching

--- 44_Wildcard Matching/main.cpp
+++ 44_Wildcard Matching/main.cpp
@@ -8,10 +8,41 @@ class Solution {
 public:
 	//字符串模式匹配问题，s为匹配串，p为模式串
 	bool isMatch(string s, string p) {
-		return isMatch2(s.c_str(), p.c_str());
+		return isMatch1(s.c_str(), p.c_str());
 	}
 
-	/*转换为字符串指针解决该问题*/
+	/*方法一：采用贪心的性质  AC*/
+	bool isMatch1(const char *s, const char *p) {
+		//? match one
+		//* match 0,1,2,3..
+		// aaaabc *c true
+		const char* star = nullptr;
+		const char* rs = nullptr;
+
+		while (*s) {
+			if (*s == *p || *p == '?') { //match
+				s++; p++;
+				continue;
+			}
+			if (*p == '*') {
+				star = p; // record star
+				p++; //match from next p
+				rs = s; // record the position of s , star match 0
+				continue;
+			}
+			if (star != nullptr) { //if have star in front then backtrace
+				p = star + 1; //reset the position of p 
+				s = rs + 1;
+				rs++; //star match 1,2,3,4,5....
+				continue;
+			}
+			return false; //if not match return false
+		}
+		while (*p == '*') p++; //skip continue star
+		return *p == '\0'; // successful match
+	}
+
+	/*方法二：利用字符串指针递归解决 性能不好 TLE*/
 	bool isMatch2(const char *s, const char *p)
 	{
 		if (p == NULL || s == NULL)
@@ -47,7 +78,7 @@ int main()
 {
 	Solution s;
 
-	cout << s.isMatch("aa", "a") << endl;
+	cout << s.isMatch("aa", "a") << endl; 
 	cout << s.isMatch("aa", "aa") << endl;
 	cout << s.isMatch("aaa", "aa") << endl;
 	cout << s.isMatch("aa", "*") << endl;
