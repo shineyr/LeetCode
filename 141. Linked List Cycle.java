4f25fb9511793f152fbb5d09941b2deee2e46e90
逆風的薔薇 <riayang.cn@gmail.com>
2020-10-13 22:37:06 -0700

    Update 141. Linked List Cycle.java

--- 141. Linked List Cycle.java
+++ 141. Linked List Cycle.java
@@ -15,6 +15,25 @@ public class Solution {
             return false;
         }
         
+        ListNode slow = head, fast = head;
+        
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if (slow == fast) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    public boolean hasCycle1(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        
         ListNode slow = head, fast = head.next;
         
         while(slow != fast) {
