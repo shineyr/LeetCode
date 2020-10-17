25a7eb7feb536b3c0e42641d721b0a1f41416106
逆風的薔薇 <riayang.cn@gmail.com>
2020-10-16 21:58:42 -0700

    Create 344. Reverse String.java

---
+++ 344. Reverse String.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseString(char[] s) {        
+        int left = 0, right = s.length - 1;
+        while(left < right) {
+            char tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+            
+            ++left;
+            --right;
+        }
+    }
+}
