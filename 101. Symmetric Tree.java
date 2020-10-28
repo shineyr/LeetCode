39d6de7e7018059a5aa011ee6c4dea884cd20b2c
逆風的薔薇 <riayang.cn@gmail.com>
2020-10-27 22:54:45 -0700

    Create 101. Symmetric Tree.java

---
+++ 101. Symmetric Tree.java
@@ -0,0 +1,70 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(root);
+        
+        while(!queue.isEmpty()) {
+            TreeNode t1 = queue.poll();
+            TreeNode t2 = queue.poll();
+            
+            if (t1 == null && t2 == null) {
+                continue;
+            }
+            
+            if (t1 == null || t2 == null) {
+                return false;
+            }
+            
+            if (t1.val != t2.val) {
+                return false;
+            }
+            
+            queue.add(t1.left);
+            queue.add(t2.right);
+            queue.add(t1.right);
+            queue.add(t2.left);
+        }
+        
+        return true;
+    }
+    
+    public boolean isSymmetric1(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        
+        return isMirror(root.left, root.right);
+    }
+    
+    private boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return true;
+        }
+        
+        if (t1 == null || t2 == null) {
+            return false;
+        }
+        
+        return t1.val == t2.val && isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+    }
+}
